477ba1de0d7555c181fc1134345e1ca07ef4978c
PRIYADHARSHIKA-02 <rithudharsh147@gmail.com>
2026-05-02 17:37:23 +0530

    feat(leetcode): add 268 - solution

---
+++ src/LeetCode/java/MissingNumber.java
@@ -0,0 +1,15 @@
+package LeetCode.java;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expected = n * (n + 1) / 2;
+        int actual = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            actual += nums[i];
+        }
+        return expected - actual;
+    
+    }
+}
