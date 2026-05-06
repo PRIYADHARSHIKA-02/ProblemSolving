7eb13cfdb8e07b1628c6a4d875eef023413c8ec4
PRIYADHARSHIKA-02 <rithudharsh147@gmail.com>
2026-05-06 21:28:21 +0530

    feat(leetcode): add 35 solution

---
+++ src/LeetCode/java/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,24 @@
+package LeetCode.java.BinarySearch;
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+
+// Time Complexity: O(log n) where n is the number of elements in the input array.
+// Space Complexity: O(1)
