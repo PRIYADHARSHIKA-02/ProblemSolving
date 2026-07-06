ddc039317a4068eac141fda06d7438824c27127a
PRIYADHARSHIKA-02 <rithudharsh147@gmail.com>
2026-07-06 23:09:27 +0530

    feat(leetcode): add Search2dMatrix, CheckIfTwoStringArraysareEquivalent, and LongestSubstringWithoutRepeatingCharacters Java solutions

---
+++ src/LeetCode/java/BinarySearch/Search2dMatrix.java
@@ -0,0 +1,25 @@
+package LeetCode.java.BinarySearch;
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=0; i < m; i++){
+            if(target >= matrix[i][0] && target <= matrix[i][n-1]){
+                int l = 0;
+                int r = n - 1;
+                while(l <= r){
+                    int mid = l + (r -l) / 2;
+                    if(matrix[i][mid] == target)
+                        return true;
+                    
+                    if(matrix[i][mid] < target){
+                        l = mid + 1;
+                    }else{
+                        r = mid - 1;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
