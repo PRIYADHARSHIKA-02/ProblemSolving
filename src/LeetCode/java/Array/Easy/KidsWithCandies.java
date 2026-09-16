00b8e0d2868b30b35f9e0c2ab0711f771ebcc11c
PRIYADHARSHIKA-02 <rithudharsh147@gmail.com>
2026-09-17 00:35:21 +0530

    feat(LeetCode): add KidsWithCandies Java solution

---
+++ src/LeetCode/java/Array/Easy/KidsWithCandies.java
@@ -0,0 +1,20 @@
+package LeetCode.java.Array.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> res = new ArrayList<>();
+        int max = 0;
+        for (int candy : candies) {
+            if (candy > max) {
+                max = candy;
+            }
+        }
+        for (int candy : candies) {
+            res.add(candy + extraCandies >= max);
+        }
+        return res;
+    }
+}
