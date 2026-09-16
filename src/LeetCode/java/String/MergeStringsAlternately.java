ccb72e90a7e2dbcd2feb2fb759c6f7354781029d
PRIYADHARSHIKA-02 <rithudharsh147@gmail.com>
2026-09-17 00:28:08 +0530

    feat(LeetCode): add MergeStringsAlternately Java solution

---
+++ src/LeetCode/java/String/MergeStringsAlternately.java
@@ -0,0 +1,20 @@
+package LeetCode.java.String;
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        StringBuilder sb = new StringBuilder(m + n);
+        int i = 0;
+        while (i < m || i < n) {
+            if (i < m) {
+                sb.append(word1.charAt(i));
+            }
+            if (i < n) {
+                sb.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return sb.toString();
+    }
+}
